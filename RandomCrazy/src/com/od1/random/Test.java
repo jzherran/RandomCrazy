a9d1ce3d4469c43e8be8a3843d7fb2d4b0a4a7d7
jzherran <jzherran@gmail.com>
2014-03-16 22:24:59 -0500

    Numbers

--- RandomCrazy/src/com/od1/random/Test.java
+++ RandomCrazy/src/com/od1/random/Test.java
@@ -8,8 +8,8 @@ public class Test implements Comparable<Test>{
 	private long id ;
 	
 	public static void main(String[] args) {
-		Integer a = 7;
-		Integer b = 5;
+		Integer a = 13;
+		Integer b = 10;
 		System.out.println(a.compareTo(b));
 		Test t1 = new Test( Integer.MAX_VALUE );
 		Test t2 = new Test( Long.MAX_VALUE );
